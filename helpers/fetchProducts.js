4f4b872665e2c142b987e2caeea2414a76a0b6a7
Diogo Martins <me.martinsfe@gmail.com>
2022-09-11 18:16:47 -0300

    refact: delete try block

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,13 +1,9 @@
 const fetchProducts = async (query) => {
   const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
 
-  try {
-    const PROMISE = await fetch(API_URL);
-    const RESPONSE = await PROMISE.json();
-    return RESPONSE;
-  } catch (error) {
-    console.error(error);
-  }
+  const PROMISE = await fetch(API_URL);
+  const RESPONSE = await PROMISE.json();
+  return RESPONSE;
 };
 
 if (typeof module !== 'undefined') {
