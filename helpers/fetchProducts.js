a46053633921bde2066f96280d9a013c78e51112
Diogo Martins <me.martinsfe@gmail.com>
2022-09-11 12:31:58 -0300

    feat(API): add fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,13 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (query) => {
+  const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+
+  try {
+    const PROMISE = await fetch(API_URL);
+    const RESPONSE = await PROMISE.json();
+    return RESPONSE.results;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 if (typeof module !== 'undefined') {
