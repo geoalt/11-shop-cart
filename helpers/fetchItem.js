4a47653e4432200d168b6eba81d93265329c9159
Diogo Martins <me.martinsfe@gmail.com>
2022-09-11 22:47:10 -0300

    feat: add fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,9 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (query) => {
+  const API_URL = `https://api.mercadolibre.com/items/${query}`;
+
+  const PROMISE = await fetch(API_URL);
+  const RESPONSE = await PROMISE.json();
+  return RESPONSE;
 };
 
 if (typeof module !== 'undefined') {
